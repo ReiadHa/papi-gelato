d1653c58b9de75f587405c4880d66983d98b4e53
Riad Hak <99054420@mydavinci.nl>
2021-12-01 14:14:58 +0100

    update

--- papigelato.py
+++ papigelato.py
@@ -1,27 +1,78 @@
-def welkom():
-    print('welkom bij Papi gelato!')
-    print('je mag alle smaken kiezen zolang dat het vanile, aardbei, chocolade en munt is')
-
-
-def sorry():
-    print('sorry dat begrijp ik niet! ')
-
-
+aantalbollen = 0
+horn = 0
+bakjes = 0
+slagroom = 0
+sprikels = 0
+caramel = 0
+slagprijs = 0.50
+sprinprijs = 0.30
+caraprijs = 0.60
 def vraag():
     vraag = input('wilt u nog meer bestellen? ')
     if vraag == 'y' or vraag == 'yes' or vraag == 'Y':
-        return bolletjes()
+        return loop()
     elif vraag == 'n' or vraag == 'no' or vraag == 'N':
         print('bedankt voor je bestelling! ')
         print('tot ziens')
     elif vraag != vraag != 'y' or vraag != 'yes' or vraag != 'Y' or vraag != 'n' or vraag != 'no' or vraag != 'N':
         sorry()
+        loop()
+
+def welkom():
+    print('Welkom bij Papi Gelato!')
+    print('je mag alle smaken kiezen zolang dat het vanile, aardbei, chocolade en munt is')
+
+def sorry():
+    print('sorry dat begrijp ik niet! ')
+
+def factuur():
+    total = ((aantalbollen*1.10)+(bakjes*0.75)+(horn*1.25)+(slagroom*slagprijs + sprikels * sprinprijs + caramel*caraprijs))
+    print('----------["Papi Gelato"]----------')
+    print('Bolletjes     {} x 1.10  = {:.2f}'.format(aantalbollen,(aantalbollen*1.10)))
+    print('Horrentje     {} x 1.25  = {:.2f}'.format(horn,(horn*1.25)))
+    print('Bakje         {} x 0.75  = {:.2f}'.format(bakjes,(bakjes*0.75)))
+    print('Toppings                = {:.2f}'.format(slagroom*slagprijs + sprikels * sprinprijs + caramel*caraprijs))
+    print('                         ------')
+    print('Totaal                  = {:.2f}'.format(total))
+    vraag()
+        
+def loop():
+    bestellingen = int(input('hoeveel beestellingen wilt u maken ?'))
+    for i in range(bestellingen):
         bolletjes()
 
-
 def bolletjes():
+    global aantalbollen
+    global horn
+    global bakjes
+    global slagprijs 
+    global caraprijs
+    global sprinprijs
+
     welkom()
     aantal = str(input('hoeveel bolletjes wilt u? '))
+    aantalbollen += int(aantal)
+    def topping():
+        global slagroom 
+        global caramel
+        global sprikels
+        top = input('wil je toppings op je ijsje? ')
+        if top == 'y' or top == 'Y' or top == 'yes' or top == 'Yes' or top == 'j' or top == 'J' or top == 'Ja' or top == 'ja':
+            topingvraag = input('wat voor topping wil je? kies een van de volgende: A)Slagroom B)Sprinkels C)Caramel saus: ')
+            if topingvraag == 'A' or topingvraag == 'a':
+                topingvraag == 'Slagroom'
+                slagroom += 1
+            elif topingvraag == 'B' or topingvraag == 'b':
+                topingvraag == 'Sprinkels'
+                sprikels += 1
+            elif topingvraag == 'C' or topingvraag == 'c':
+                topingvraag == 'Caramel'
+                caramel += 1
+        elif top == 'n' or top == 'N' or top =='Nee' or top == 'nee':
+            smaken()
+        else: 
+            sorry()
+            topping()
     def smaken():
         nummer = 0
         for x in range(int(aantal)):
@@ -38,22 +89,29 @@ def bolletjes():
             else:
                 sorry()
                 smaken()
-    smaken()
-
-          
+    
+    smaken() 
+    topping()     
     if aantal.isdigit():
         aantal = int(aantal)
         if aantal <= 3:
-            keuze = input('wilt u deze {} bolletjes in A) een hoorntje of b) een bakje? '.format(aantal))
+            keuze = input('wilt u deze {} bolletjes in A) een Bakje of b) een Hoorntje? '.format(aantal))
             bak = 'bak'
             hoornjte = 'hoorntje'
             if keuze == 'a' or keuze == 'A':
+                slagprijs = 0.90
+                caraprijs = 0.90
+                sprinprijs = 0.90
+
                 print('dan krijgt u van mij {} bolletjes in een {}'.format(aantal, bak))
-                print('hier is uw {} in een {}'.format(aantal, hoornjte))
-            if keuze == 'b' or keuze == 'B':
+                print('hier is uw {} in een {}'.format(aantal, bak))
+
+                bakjes += 1
+            elif keuze == 'b' or keuze == 'B':
                 print('dan krijgt u van mij {} bolletjes in een {}'.format(aantal, hoornjte))
-                print('hier is uw {} bolletjes in een {}'.format(aantal, bak))
-            elif keuze != keuze != 'a' or keuze != 'A' or keuze != 'b' or keuze != 'B':
+                print('hier is uw {} bolletjes in een {}'.format(aantal, hoornjte))
+                horn += 1   
+            else:
                 sorry()
                 smaken()
         if 4 <= aantal <= 8:
@@ -64,6 +122,5 @@ def bolletjes():
         sorry()
         bolletjes()
 
-
-bolletjes()
-vraag()
+loop()
+factuur()
--- try31.py
+++ try31.py
@@ -1,24 +1,12 @@
-def welkom():
-    print('Welkom bij Papi Gelato!')
-    print('je mag alle smaken kiezen zolang dat het vanile, aardbei, chocolade en munt is')
-
-
-def sorry():
-    print('sorry dat begrijp ik niet! ')
-
-def factuur():
-        print('----------["Papi Gelato"]----------')
-        print('Bolletjes     ',aantalbollen, 'x', 5.50, ' =',  aantalbollen * 5.50 )
-        print('Horrentje     ',1, 'x', horn,'=',       1 * horn )
-        print('Bakje         ',0, 'x', bakjes, '=',       0.75 * bakjes)
-        print('                         ------')
-        print('Totaal                  = ',(aantalbollen*1.10)+(bakjes*0.75)+(horn*1.25))
-        
-def loop():
-    bestellingen = int(input('hoeveel beestellingen wilt u maken ?'))
-    for i in range(bestellingen):
-        bolletjes()
-
+aantalbollen = 0
+horn = 0
+bakjes = 0
+slagroom = 0
+sprikels = 0
+caramel = 0
+slagprijs = 0.50
+sprinprijs = 0.30
+caraprijs = 0.60
 def vraag():
     vraag = input('wilt u nog meer bestellen? ')
     if vraag == 'y' or vraag == 'yes' or vraag == 'Y':
@@ -29,17 +17,62 @@ def vraag():
     elif vraag != vraag != 'y' or vraag != 'yes' or vraag != 'Y' or vraag != 'n' or vraag != 'no' or vraag != 'N':
         sorry()
         loop()
-aantalbollen = 0
-horn = 0
-bakjes = 0
+
+def welkom():
+    print('Welkom bij Papi Gelato!')
+    print('je mag alle smaken kiezen zolang dat het vanile, aardbei, chocolade en munt is')
+
+def sorry():
+    print('sorry dat begrijp ik niet! ')
+
+def factuur():
+    total = ((aantalbollen*1.10)+(bakjes*0.75)+(horn*1.25)+(slagroom*slagprijs + sprikels * sprinprijs + caramel*caraprijs))
+    print('----------["Papi Gelato"]----------')
+    print('Bolletjes     {} x 1.10  = {:.2f}'.format(aantalbollen,(aantalbollen*1.10)))
+    print('Horrentje     {} x 1.25  = {:.2f}'.format(horn,(horn*1.25)))
+    print('Bakje         {} x 0.75  = {:.2f}'.format(bakjes,(bakjes*0.75)))
+    print('Toppings                = {:.2f}'.format(slagroom*slagprijs + sprikels * sprinprijs + caramel*caraprijs))
+    print('                         ------')
+    print('Totaal                  = {:.2f}'.format(total))
+        
+def loop():
+    bestellingen = int(input('hoeveel beestellingen wilt u maken ?'))
+    for i in range(bestellingen):
+        bolletjes()
+    vraag()
 
 def bolletjes():
     global aantalbollen
     global horn
     global bakjes
+    global slagprijs 
+    global caraprijs
+    global sprinprijs
+
     welkom()
     aantal = str(input('hoeveel bolletjes wilt u? '))
     aantalbollen += int(aantal)
+    def topping():
+        global slagroom 
+        global caramel
+        global sprikels
+        top = input('wil je toppings op je ijsje? ')
+        if top == 'y' or top == 'Y' or top == 'yes' or top == 'Yes' or top == 'j' or top == 'J' or top == 'Ja' or top == 'ja':
+            topingvraag = input('wat voor topping wil je? kies een van de volgende: A)Slagroom B)Sprinkels C)Caramel saus: ')
+            if topingvraag == 'A' or topingvraag == 'a':
+                topingvraag == 'Slagroom'
+                slagroom += 1
+            elif topingvraag == 'B' or topingvraag == 'b':
+                topingvraag == 'Sprinkels'
+                sprikels += 1
+            elif topingvraag == 'C' or topingvraag == 'c':
+                topingvraag == 'Caramel'
+                caramel += 1
+        elif top == 'n' or top == 'N' or top =='Nee' or top == 'nee':
+            smaken()
+        else: 
+            sorry()
+            topping()
     def smaken():
         nummer = 0
         for x in range(int(aantal)):
@@ -57,7 +90,8 @@ def bolletjes():
                 sorry()
                 smaken()
     
-    smaken()      
+    smaken() 
+    topping()     
     if aantal.isdigit():
         aantal = int(aantal)
         if aantal <= 3:
@@ -65,10 +99,14 @@ def bolletjes():
             bak = 'bak'
             hoornjte = 'hoorntje'
             if keuze == 'a' or keuze == 'A':
+                slagprijs = 0.90
+                caraprijs = 0.90
+                sprinprijs = 0.90
+
                 print('dan krijgt u van mij {} bolletjes in een {}'.format(aantal, bak))
                 print('hier is uw {} in een {}'.format(aantal, bak))
+
                 bakjes += 1
-            
             elif keuze == 'b' or keuze == 'B':
                 print('dan krijgt u van mij {} bolletjes in een {}'.format(aantal, hoornjte))
                 print('hier is uw {} bolletjes in een {}'.format(aantal, hoornjte))
@@ -76,7 +114,6 @@ def bolletjes():
             else:
                 sorry()
                 smaken()
-
         if 4 <= aantal <= 8:
             print('dan krijgt u van mij een bakje met {} bolletjes'.format(aantal))
         elif aantal >= 9:
@@ -87,4 +124,3 @@ def bolletjes():
 
 loop()
 factuur()
-vraag()
